78ed6be3519691fb210beab20c86fb8acf0a75ae
vbvss199 <vbvss1999@gmail.com>
2025-07-01 00:33:03 -0400

    finally solved the roman numeral

--- roman_numeral.dart
+++ roman_numeral.dart
@@ -6,7 +6,6 @@ void main(){
 void romanToInt(String s) {
   //var I =1,V=5,X=10,L=50,C=100,D=500,M=1000;
   int sum=0;
-  int difference=0;
   var romans={
     'I':1,
     'V':5,
@@ -16,24 +15,41 @@ void romanToInt(String s) {
     "D":500,
     "M":1000
   };
-  List<String> nums=s.split("");
-  List<int> nums_int=[];
-  nums.forEach((String ele){
-    nums_int.add(romans[ele]!);
+
+int checkOrder(List<String> t){
+  if (romans[t[0]]! >= romans[t[1]]!){
+    return romans[t[0]]!+romans[t[1]]!;
+  }
+  else{
+     return romans[t[1]]!-romans[t[0]]!;
+  }
+}
+
+List<int> nums_int_mod=[];
+// special function 
+List<int> specialFunction(List<String> l){
+  l.forEach((String element){
+    List<String> test = element.split("");
+    nums_int_mod.add(checkOrder(test));
   });
-  //print(nums_int);
-  // order does matter
-    if (nums_int[0]<nums_int[1] ){
-      nums_int.forEach((ele){
-        difference=ele-difference;
-      });
-    }
-  //order doesn't matter 
-    else{
-      nums_int.forEach((ele){
-        sum=sum+ele;
-      });
-    }
-  print(difference);
+  return nums_int_mod;
+  }
+
+  // splitting to first character and two charcters each 
+  String firstCharcter=s[0];
+  String remainingCharcter=s.substring(1);
+  List<String> nums_mod=[firstCharcter];
+  for(int i=0;i<remainingCharcter.length;i+=2){
+    int endIndex=i+2;
+    nums_mod.add(remainingCharcter.substring(i,endIndex));
+  }
+  // now check the list numbers sum separately from 2nd number whether order matters or not  if order matters calculate the difference else calculate the sum and return the array with the respective elements
+  // just like nums_mod=["M","CM","XC","IV"] should return [1000,900,90,4] lastly add this thing
+  nums_int_mod.add(romans[nums_mod[0]]!);
+  List<int> finalList=specialFunction(nums_mod.sublist(1));
+  print(finalList);
+  finalList.forEach((ele){
+    sum+=ele;
+  });
   print(sum);
   }
