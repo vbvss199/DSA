91195bf3d42a4ffc586ad0de613f02fd28b1ee7b
vbvss199 <vbvss1999@gmail.com>
2025-12-07 21:22:51 -0500

    comments to the code:

--- arrays/nextelementinanarrayusingpermutations.py
+++ arrays/nextelementinanarrayusingpermutations.py
@@ -11,15 +11,15 @@ class Solution:
             if nums[i] < nums[i + 1]:
                 index = i
                 break
+        # that means array should be reversed
         if index == -1:
             return nums.reverse()
             return
+        #  2 swap the index element with the one less than the index comparing to the right
+        # 3 finally reverse the array nxt to the i i.e from i+1
         else:
             for i in range(len(nums) - 1, -1, -1):
                 if nums[i] > nums[index]:
                     nums[i], nums[index] = nums[index], nums[i]
                     break
             nums[index + 1 :] = nums[index + 1 : len(nums)][::-1]
-
-        #  2 swap the index element with the one less than the index comparing to the right
-        # 3 finally reverse the array nxt to the i i.e from i+1
